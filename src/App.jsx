d7b9b07158f334513bd2f7c687e08c47884c2084
Ngoy banza <dngoyb@gmail.com>
2021-11-09 20:10:16 +0200

    adding and deleting from the database

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,30 @@ function App() {
 	};
 
 	// Add task
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task };
+	const addTask = async (task) => {
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(task),
+		});
 
-		setTasks([...tasks, newTask]);
+		const data = await res.json();
+
+		setTasks([...tasks, data]);
+
+		// const id = Math.floor(Math.random() * 10000) + 1;
+		// const newTask = { id, ...task };
+
+		// setTasks([...tasks, newTask]);
 	};
 
 	// Delete task
-	const deleteTask = (id) => {
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE',
+		});
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
